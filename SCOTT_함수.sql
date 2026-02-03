f8a0c209848458e6599c007c60a72ef36b96c244
kimeunji806 <sacri8fice@gmail.com>
2026-02-03 17:48:16 +0900

    260203수업

--- SCOTT_함수.sql
+++ SCOTT_함수.sql
@@ -99,6 +99,7 @@ WHERE deptno = 20;
 SELECT name
       ,jumin
       ,replace(jumin, substr(jumin,7), '-/-/-/-') replace
+      ,replace(jumin, substr(jumin,7), '-')
 FROM student
 WHERE deptno1 = 101;
 
@@ -117,3 +118,150 @@ SELECT name
       ,substr(tel,-4, 4) sbustr
 FROM student
 WHERE deptno1 = 101;
+
+-- round(123.4)
+SELECT round(123.456, -2) round
+      ,trunc(123.4567, -1) trunc
+      ,mod(12,5) mod
+      ,ceil(12.3) ceil
+      ,floor(12.3) floor
+      ,to_char(trunc(sysdate, 'hh'), 'rrrr/mm/dd hh24:mi:ss') trunc2 -- 년/월/일 시/분/초
+FROM dual;
+
+-- 날짜 관련 함수.
+SELECT ADD_MONTHS(sysdate, -1) next_month -- 28
+      ,months_between(sysdate + 28, sysdate) months
+FROM dual;
+
+-- 사원번호, 이름, 근속년수 (23년 7개월)
+SELECT empno
+      ,ename
+      ,hiredate
+      ,trunc((months_between(sysdate,hiredate))/12) || '년 '
+      || mod(trunc((months_between(sysdate,hiredate))),12) || '개월' 근속년수
+FROM emp;
+
+-- 교수번호, 이름, 입사일자, 급여 (30년 이상)
+SELECT profno
+      ,name
+      ,hiredate
+      ,pay
+      ,trunc(months_between(sysdate, hiredate)/12) 근속년수
+      ,p.deptno
+FROM professor p
+WHERE trunc(months_between(sysdate, hiredate)/12) >= 30
+ORDER BY 3;
+
+-- 교수번호, 이름, 입사일자, 급여 (20년 이상, Software Engineering)
+SELECT profno
+      ,name
+      ,hiredate
+      ,pay
+      ,position
+      ,p.deptno "P.deptno"
+      ,d.deptno "D.deptno"
+      ,d.dname
+FROM professor p, department d
+WHERE trunc(months_between(sysdate, hiredate)/12) >= 20
+AND d.dname = 'Software Engineering'
+AND d.deptno = p.deptno; -- 두 테이블간의 equal 조건.
+
+--SALES부서에서 근속년수 40년이 넘는 사람. 사번, 이름, 급여, 부서명
+SELECT e.empno
+      ,e.ename
+      ,e.sal
+      ,d.dname
+FROM emp e, dept d
+WHERE e.deptno = d.deptno
+AND d.dname = 'SALES'
+AND (months_between(sysdate, hiredate)/12) >= 40
+ORDER BY e.empno;
+
+-- 학과.
+SELECT *
+FROM department;
+
+SELECT profno, name, p.deptno, d.deptno, dname
+FROM professor p, department d
+WHERE p.deptno = d.deptno
+AND d.dname = 'Computer Engineering'; -- 16*12=192
+
+--2 + '2'
+SELECT 2 + to_number('2', 9)
+      ,concat(2, '2')
+      ,sysdate
+FROM dual
+WHERE sysdate > '2026/02/03';
+
+-- to_char(날짜, '포맷문자')
+SELECT sysdate
+      ,to_char(sysdate, 'RRRR-MM-DD HH24:MI:SS') to_char
+      ,to_date('05/2024/03', 'MM/RRRR/DD') to_date
+FROM dual;
+
+-- to_char
+SELECT to_char(12345.6789, '099,999.99') -- 반올림한 연산 결과를 문자로 출력.
+FROM dual;
+
+-- p.105 형 변환 함수 퀴즈
+SELECT studno
+      ,name
+      ,to_char(birthday,'dd/mon/rr') birthday
+FROM student
+WHERE to_char(birthday,'mm') = '01';
+
+-- nvl()
+SELECT nvl(10,0) -- null ? 0 : 10
+FROM dual;
+
+SELECT pay + nvl(bonus,0) "월급"
+FROM professor;
+
+-- student(profno) -> 9999(없으면)/담당교수번호
+--                    담당교수 없음/담당교수번호
+SELECT name
+      ,nvl(profno,9999) prof1
+      ,nvl(to_char(profno),'담당교수없음') prof2 -- null값 -> 치환하는 것
+FROM student
+ORDER BY 2 desc;
+
+-- decode(A, B, '같은조건', '다른조건')
+SELECT decode(10, 20, '같다', '다르다') -- 10 == 20 ? '같다' : '다르다'
+FROM dual;
+
+SELECT studno, profno, decode(profno, null, 9999, profno)
+FROM student
+ORDER BY profno desc;
+
+SELECT decode('C', 'A', '현재A', 'B', '현재B', '기타')
+FROM dual;
+
+-- p.114 예제1)
+SELECT deptno
+      ,name
+      ,decode(deptno, 101, 'Computer Engineering') DNAME
+FROM professor;
+
+-- p.115 예제2)
+SELECT deptno
+      ,name
+      ,decode(deptno, 101, 'Computer Engineering', 'ETC') DNAME
+FROM professor;
+
+-- p.116 예제3)
+SELECT deptno
+      ,name
+      ,decode(deptno, 101, 'Computer Engineering', 102, 'Multimedia Engineering', 103, 'Software Engineering', 'ETC') DNAME
+FROM professor;
+
+-- p.117 예제4)
+SELECT deptno
+      ,name
+      ,decode(deptno, 101, decode(name, 'Audie Murphy', 'BEST!')) ETC
+FROM professor;
+
+-- p.118 예제5)
+SELECT deptno
+      ,name
+      ,decode(deptno, 101, decode(name, 'Audie Murphy', 'BEST!','GOOD!')) ETC
+FROM professor;
--- TODO.sql
+++ TODO.sql
@@ -1,3 +1,37 @@
 --TODO.sql
 SELECT ename || '''s' || ' sal is ' || '$' || sal as "Name And Sal"
 FROM emp;
+
+-- 2026.02.03(화) TODO.
+
+-- 107page.
+SELECT empno
+      ,ename
+      ,sal
+      ,comm
+      ,to_char((sal*12)+comm,'999,999') salpay
+FROM emp
+WHERE ename = 'ALLEN';
+
+SELECT name
+      ,pay
+      ,bonus
+      ,to_char((pay*12)+bonus,'999,999') totpay
+FROM professor
+WHERE deptno = 201;
+
+-- 108page.
+
+-- 113page(nvl2).
+
+-- 학생테이블의 생년월일을 기준으로 1~3 => 1/4분기.
+--                             4~6 => 2/4분기.
+--                             7~9 => 3/4분기.
+--                             10~12 => 4/4분기.
+SELECT name
+      ,birthday
+      ,decode(to_char(birthday,'mm'),1, '1/4분기', 2, '1/4분기', 3, '1/4분기',
+      (decode(to_char(birthday,'mm'),  4, '1/4분기', 5, '2/4분기', 6, '2/4분기',
+      (decode(to_char(birthday,'mm'), 7, '3/4분기', 8, '3/4분기', 9, '3/4분기',
+      (decode(to_char(birthday,'mm'), 10, '4/4분기', 11, '4/4분기', 12, '4/4분기'))))))) "생년월일"
+FROM student;
